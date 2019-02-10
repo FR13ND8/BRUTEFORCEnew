d436abfd4b9e50b0c8fb0ac663eec14eed39c9c9
Riski Darmawan <46347378+FR13ND8@users.noreply.github.com>
2019-02-10 18:20:20 +0000

    Add files via upload

---
+++ run.py
@@ -0,0 +1,30 @@
+import random
+import sys
+import time
+import os
+
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.2)
+print (' ')
+mengetik(i+'MAAF TOOL INI SUDAH DI PERBARUI')
+mengetik('SELAMAT MENIKMATI TOOL PEMBARUAN')
+mengetik('KARENA SAYA MELIHAT BANYAK PERSALAHAN DI TOOL LAMA')
+mengetik('MAKA DARI ITU SAYA PERBARUI TOOL INI')
+os.system ('sleep 1')
+print (' ')
+mengetik (cy+'KAMI AKAN MENGINSTALLNYA LAGI DALAM WAKTU 5 DETIK')
+mengetik (me+'WELCOME TO'+ ku + ' BRUTEFORCEnew ^_^ ')
+os.system ('sleep 1')
+print (' ')
+mengetik('Loading °°°°°°°°°°°°°°°°°°°°°')
+os.system ('sleep 1')
